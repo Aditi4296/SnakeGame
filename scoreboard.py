a3f4142cf7073a2b818233ff1cbfa4ff87b19c90
Aditi4296 <aditisingh4296@rediffmail.com>
2023-07-21 00:32:46 +0530

    snake extensions added

--- scoreboard.py
+++ scoreboard.py
@@ -20,3 +20,7 @@ class ScoreBoard(Turtle):
         self.clear()
         self.score += 1
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
--- snake.py
+++ snake.py
@@ -17,13 +17,7 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            # new_segment.goto(x=x_index, y=0)
-            # x_index -= 20
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
     def move(self):
         # for seg_num in range(start=2 , stop=0, step=-1 ):
@@ -50,3 +44,17 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        # new_segment.goto(x=x_index, y=0)
+        # x_index -= 20
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+
+    def extend(self):
+        # add a new segment to the snake
+        self.add_segment(self.segments[-1].position())
